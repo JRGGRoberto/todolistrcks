6ce7513b75484c12f5431d5bed68e03717041085
Roberto <jrggroberto@gmail.com>
2023-10-11 14:00:14 -0300

    :lock: feat add hash on saved password

--- src/main/java/br/com/jrggroberto/todolist/user/UserController.java
+++ src/main/java/br/com/jrggroberto/todolist/user/UserController.java
@@ -1,13 +1,13 @@
 package br.com.jrggroberto.todolist.user;
 
-import org.jcp.xml.dsig.internal.SignerOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -26,6 +26,9 @@ public class UserController {
       return ResponseEntity.status(400).body("Usuário já existe");
 
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(200).body(userCreated);
