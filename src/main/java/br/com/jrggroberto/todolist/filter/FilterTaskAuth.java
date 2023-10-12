5c8843e2ec6274b13adeed32652b93934797ac06
Roberto <jrggroberto@gmail.com>
2023-10-12 14:24:24 -0300

    :goal_net: feat - completed class 3

--- src/main/java/br/com/jrggroberto/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/jrggroberto/todolist/filter/FilterTaskAuth.java
@@ -4,15 +4,22 @@ import java.io.IOException;
 import java.util.Base64;
 
 import jakarta.servlet.FilterChain;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.jrggroberto.todolist.user.IUserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository uIUserRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -23,8 +30,8 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     // Pegar a autenticação (user)
     var authorization = request.getHeader("Authorization").substring(6);
 
+    // System.out.println(authorization);
     // pegar parte útil: retorno :"Basic anJnZ3JvYmVydG86MTIzNDU="
-    System.out.println(authorization);
 
     byte[] authDecode = Base64.getDecoder().decode(authorization);
     var authString = new String(authDecode);
@@ -37,12 +44,20 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     System.out.println(password);
 
     // Validar usuário
+    var user = this.uIUserRepository.findByUsername(username);
 
-    System.out.println("chegou ao filtro");
+    if (user == null) {
+      response.sendError(401);
+    } else {
+      // Validar senha
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+      if (passwordVerify.verified) {
+        filterChain.doFilter(request, response);
+      } else {
+        response.sendError(401);
+      }
 
-    //
-    // throw new UnsupportedOperationException("Unimplemented method
-    // 'doFilterInternal'");
+    }
 
   }
 
