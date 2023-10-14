627258442f162ba509a88ef00b7f558bc00b60ce
Roberto <jrggroberto@gmail.com>
2023-10-13 22:10:00 -0300

    :goal_net: feat get all tasks by user

--- src/main/java/br/com/jrggroberto/todolist/task/TaskController.java
+++ src/main/java/br/com/jrggroberto/todolist/task/TaskController.java
@@ -1,12 +1,13 @@
 package br.com.jrggroberto.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +24,8 @@ public class TaskController {
 
   @PostMapping("/")
   public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-    var idUs = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID) idUs);
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
 
     var currentDate = LocalDateTime.now();
     if (currentDate.isAfter(taskModel.getStartAt())) {
@@ -38,6 +39,13 @@ public class TaskController {
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(200).body(task);
 
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
   }
 
 }
